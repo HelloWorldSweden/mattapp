5052608c2812794fdc86e1b59bc947543b55f7bf
Mats Larsson <m.larsson88@gmail.com>
2018-10-04 14:41:50 +0200

    much done, much to do

--- mattapp.py
+++ mattapp.py
@@ -14,15 +14,8 @@ import tkinter as tk
 from random import *
 
 
-def combine_funcs(*funcs):
-    def combined_func(*args, **kwargs):
-        for f in funcs:
-            f(*args, **kwargs)
-    return combined_func
-
-
-LARGE_FONT = ("Verdana", 12)
-NORM_FONT = ("Verdana", 10)
+LARGE_FONT = ("ms serif", 20)
+NORM_FONT = ("ms serif", 12)
 SMALL_FONT = ("Verdana", 8)
 
 style.use("ggplot")
@@ -30,14 +23,34 @@ style.use("ggplot")
 f = plt.figure()
 a = f.add_subplot(1,1,1)
 
+
+def combine_funcs(*funcs):
+    def combined_func(*args, **kwargs):
+        for f in funcs:
+            f(*args, **kwargs)
+    return combined_func
+
+
+# i for intervall
+def animate(i):
+
+    bins = 11
+    dice_rolled= Dice.stat
+    a.clear()
+    a.hist(dice_rolled, bins, rwidth=0.8, color="skyblue")
+
+
+
+
 class Dice(tk.Tk):
-    stat = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    stat = []
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, *kwargs)
 
+
         self.wm_title("Dices")
 
-
+        self.i=0
         # StringVar() updates result label automatically
         self.text = tk.StringVar()
 
@@ -45,22 +58,22 @@ class Dice(tk.Tk):
         self.text.set("")
 
         # create the result label
-        result = ttk.Label(self, textvariable=self.text)
-        result.grid(row=3, column=0, columnspan=3)
+        #result = ttk.Label(self, textvariable=self.text)
+        #result.grid(row=3, column=0, columnspan=3)
 
         self.dice_list = self.create_dice()
         self.dice_list2 = self.create_dice2()
 
         # start with an empty canvas
-        self.dice_list[0].grid(row=1, column=0, columnspan=3)
-        self.dice_list2[0].grid(row=1, column=1, columnspan=3)
+        self.dice_list[0].grid(row=1, column=0, columnspan=3, padx=5, pady=3)
+        self.dice_list2[0].grid(row=1, column=1, columnspan=3, padx=5, pady=3)
         button = ttk.Button(self, text="Roll", command=combine_funcs(self.click, self.click2))
-        button.grid(row=2, column=0, padx=3, pady=3)
-        button1 = ttk.Button(self, text="Roll", command=combine_funcs(self.click_fast1, self.click_fast2))
-        button1.grid(row=2, column=1, pady=3)
+        button.grid(row=2, column=0, columnspan=2, padx=5, pady=3)
+        button1 = ttk.Button(self, text="Fast roll", command=combine_funcs(self.click_fast1, self.click_fast2))
+        button1.grid(row=2, column=2, columnspan=2, padx=5, pady=3)
         button2 = ttk.Button(self, text="Quit", command=self.destroy)
-        button2.grid(row=3, column=0, pady=3)
-        #Dice.stat = []
+        button2.grid(row=3,column=0, columnspan=2, padx=5, pady=3)
+
 
 
     def create_dice(self):
@@ -76,7 +89,6 @@ class Dice(tk.Tk):
         return dice
 
     def create_dice2(self):
-
         dice = list()
         dice.append(self.draw_dice2('dot0'))  # empty
         dice.append(self.draw_dice2('dot4'))  # center dot --> 1
@@ -106,12 +118,11 @@ class Dice(tk.Tk):
         }
 
         for arg in args:
-            dots.get(arg)(x, y, r) # Gets the dictionary keys while passing in x, y, and r values
+            dots.get(arg)(x, y, r) # Gets the diccolumspan=2,tionary keys while passing in x, y, and r values
 
         return c
 
     def draw_dice2(self, *args):
-
         w, h = 23, 23 # sets width and height
         x, y, r = 2, 2, 5 # sets x, y, and radius
         c = tk.Canvas(self, width=w, height=h, bg='white') # creates canvas c
@@ -135,12 +146,14 @@ class Dice(tk.Tk):
 
 
     def click(self):
+        self.i += 1
+        print(self.i)
         self.dice1 = 0
         t = 100 # start with a time delay of 100 ms and increase it as the dice rolls
         stop = randint(13, 18) # chooses random number between 13 - 17
         for x in range(stop):
             dice_index = x % 6 + 1 # gets the randomly selected dice head by modulo
-            self.dice_list[dice_index].grid(row=1, column=0, columnspan=3)
+            self.dice_list[dice_index].grid(row=1, column=0, columnspan=3, padx=5, pady=3)
             self.update()
             if x == stop - 1:
                 # set text to the selected result
@@ -158,7 +171,7 @@ class Dice(tk.Tk):
         stop = randint(13, 18) # chooses random number between 13 - 17
         for x in range(stop):
             dice_index = x % 6 + 1 # gets the randomly selected dice head by modulo
-            self.dice_list2[dice_index].grid(row=1, column=1, columnspan=3)
+            self.dice_list2[dice_index].grid(row=1, column=1, columnspan=3, padx=5, pady=3)
             self.update()
             if x == stop - 1:
                 # set text to the selected result
@@ -171,7 +184,9 @@ class Dice(tk.Tk):
         print(self.dice2)
         Dice.stat.append(self.dice1 + self.dice2)
         print(self.stat)
-
+        if(self.i == 1):
+            self.popup_problem()
+            print(self.i)
 
     def click_fast1(self):
         self.dice1 = 0
@@ -192,18 +207,31 @@ class Dice(tk.Tk):
         Dice.stat.append(self.dice1 + self.dice2)
         print(self.stat)
 
-#    def choose(self):
-#        self.dice2 =
+    def popup_problem(self):
+        popup = tk.Tk()
+        def get_value():
+            ans = e1.get()
+            if (ans=="1/6"):
+                result1 = ttk.Label(popup, text="Rätt!", font=LARGE_FONT)
+                result1.grid(row=3, column=0, columnspan=2)
+            else:
+                result2 = ttk.Label(popup, text="Det var tyvärr fel. Prova igen!")
+                result2.grid(row=3, column=0, columnspan=2)
 
-# i for intervall
-def animate(i):
+        popup.wm_title("Problem")
+        label1 = ttk.Label(popup, text="Hur stor är sannolikheten att kasta två ettor nästa gång du kastar?", font=NORM_FONT)
+        label1.grid(row=0, columnspan=2, padx=3, pady=3, sticky="w")
+        label2 = ttk.Label(popup, text="Svar: ", font=NORM_FONT)
+        label2.grid(row=1, column=0, sticky="w", padx=3, pady=3)
 
-    bins = 11
-    dice_rolled= Dice.stat
-    a.clear()
-    a.hist(dice_rolled, bins)
+        e1 = ttk.Entry(popup)
+        e1.grid(row=1, column=0, sticky="e")
 
-
+        button1 = ttk.Button(popup, text="Svara", command=get_value)
+        button1.grid(row=1, column=1, padx=3, pady=3, sticky="e")
+        button2 = ttk.Button(popup, text="Quit", command=popup.destroy)
+        button2.grid(row=2, column=1, padx=3, pady=3, sticky="e")
+        popup.mainloop()
 
 class MattApp(tk.Tk):
 
@@ -237,7 +265,8 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Matteverkstan", font=LARGE_FONT)
+        self.configure(background='white')
+        label = ttk.Label(self, text="Matteverkstan", font=LARGE_FONT, background='white')
         label.pack(pady=10,padx=10)
 
         button1 = ttk.Button(self, text="Diagram", command=lambda: controller.show_frame(Graph))
